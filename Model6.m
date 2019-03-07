c354340aa1dd260cc279eda4c8d3df14310a3f3e
Kaitoukiddo <sherrylavender0314@gmail.com>
2019-03-07 18:10:26 -0500

    fixed current models, model6 still a bit weired

--- Model6.m
+++ Model6.m
@@ -1,4 +1,4 @@
-function [dQdt] = Model6[t, Q]
+function [dQdt] = Model6(t, Q)
 dQdt = zeros(5,1);
 u = 860/60;
 k = 61;
@@ -10,4 +10,4 @@ dQdt(1) = k*u - ka1*Q(1);
 dQdt(2) = (1-k)*u - ka2*Q(2);
 dQdt(3) = ka1*Q(1) - ka1*Q(3);
 dQdt(4) = ka2*Q(2) - ka2*Q(4);
-dQdt(5) = ka1*Q(4) + ka2*Q(4) - ke*Q(5);
+dQdt(5) = ka1*Q(3) + ka2*Q(4) - ke*Q(5);
