9d3121eb124d6cbc0b9826d5ed01022126d686ae
Kaitoukiddo <sherrylavender0314@gmail.com>
2019-03-08 14:50:14 -0500

    fixed model 10

--- Model10.m
+++ Model10.m
@@ -11,4 +11,4 @@ Vmax = 1140;
 dQdt(1) = k*u - ka1*Q(1) - Vmax*Q(1)/(km+Q(1));
 dQdt(2) = (1-k)*u - ka2*Q(2) - Vmax*Q(1)/(km+Q(2));
 dQdt(3) = ka1*Q(1) - ka1*Q(3);
-dQdt(4) = ka1*Q(3) + ka2*(2) - ke*Q(4);
+dQdt(4) = ka1*Q(3) + ka2*Q(2) - ke*Q(4);
